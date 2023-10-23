33d1c2c114ac316c661b5c8449ae042a5ae1d36e
Raeshal <raeshalkhurana07@gmail.com>
2023-10-23 23:40:59 +0530

    Changes to the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { LandingComponent } from './landing/landing.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
+import { ConnectComponent } from './connect/connect.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ 
+  {
+    path : '',
+    component: LandingComponent
+  },
+  {
+    path : 'about',
+    component: AboutComponent
+
+  },
+  {
+    path : 'experience',
+    component: ExperienceComponent
+
+  },
+  {
+    path : 'education',
+    component : EducationComponent
+
+  },
+  {
+    path : 'projects',
+    component : ProjectsComponent
+
+  },
+  {
+    path : 'expertise',
+    component : ExpertiseComponent
+
+  },
+  {
+    path : 'connect',
+    component : ConnectComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [AboutComponent ,ConnectComponent, ExperienceComponent,EducationComponent , ProjectsComponent , ExpertiseComponent ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule ,  routingComponents, } from './app-routing.module';
+import { AppComponent  } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+import { RouterModule } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
+import { LandingComponent } from './landing/landing.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
+import { ConnectComponent } from './connect/connect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+   
+    AppComponent,
+    routingComponents,
+    AboutComponent,
+    ExperienceComponent,
+    LandingComponent,
+    EducationComponent,
+    ProjectsComponent,
+    ExpertiseComponent,
+    ConnectComponent,
+    
+    
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,AboutComponent,ExperienceComponent]
 })
 export class AppModule { }
